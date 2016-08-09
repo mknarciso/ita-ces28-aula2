51117159397ec8722511a20aae681852b3867fc6
mknarciso <murilokn@gmail.com>
2016-08-09 13:03:07 +0000

    V1

---
+++ src/main/java/org/ita/comp/source/Person.java
@@ -0,0 +1,52 @@
+package org.ita.comp.source;
+import java.util.*;
+
+public class Person {
+	static int nextId=0;
+	//Changed Id from public to private, and moved it to the top 
+	private int id;
+	
+	Person(){
+		id = nextId++;
+	}
+	
+	boolean participate(Project p) {
+		List<Person> x = p.getParticipants();
+		for(int i=0; i<p.getNumberOfParticipants(); i++) {
+		     if (x.get(i).id == this.getId()) return (true);
+	     }
+	    return (false);
+	}
+	   
+	public int getId() {
+	  return id;
+	}
+	
+	
+		
+	public static void main(String[] args) {
+		Person[] ps = new Person[3];
+		for (int i=0; i < ps.length; i++)
+			ps[i] = new Person();
+		
+		Project pj = new Project();
+		pj.setParticipants(ps);
+		pj.printParticipants();
+		
+		Person x = new Person();
+		if (x.participate(pj)) {
+			System.out.println("Person "+x.id+" belongs to project");
+		} else {
+			System.out.println("Person "+x.id+" does not belong to project");
+		}
+		Person p = ps[1];
+		if (p.participate(pj)) {
+			System.out.println("Person "+p.id+" belongs to project");
+		} else {
+			System.out.println("Person "+p.id+" does not belong to project");
+		}
+		
+
+	}
+
+}
---
+++ src/main/java/org/ita/comp/source/Project.java
@@ -0,0 +1,23 @@
+package org.ita.comp.source;
+import java.util.*;
+
+class Project {
+ 
+ //Changed from array to List
+  private List<Person> participants = new ArrayList<Person>();
+  
+  //Changed return an method type
+  public List<Person> getParticipants() { return Collections.unmodifiableList(participants); }
+  //Changed lenght for size
+  public int getNumberOfParticipants() { return participants.size(); }
+  
+  //modify input type from array to list
+  public void setParticipants(Person[] p) { participants=Arrays.asList(p); }
+
+  public void printParticipants() {
+	  for (int i=0; i < participants.size(); i++)
+	      // Changed .id for .getId(), and [i] for get(i)
+		  System.out.println("project has person "+participants.get(i).getId());
+  }
+}
+
